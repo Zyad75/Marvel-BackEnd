8c99cf8ede978b29086497f30d3de3d83729a898
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-11-28 16:21:50 +0100

    last commit with query of comics route

--- routes/comics.js
+++ routes/comics.js
@@ -3,9 +3,16 @@ const axios = require("axios");
 const router = express.Router();
 const apiKey = process.env.API_KEY;
 router.get("/comics", async (req, res) => {
+  let filters = "";
+  if (req.query.skip) {
+    filters += `&skip=${req.query.skip}`;
+  }
+  if (req.query.title) {
+    filters += `&title=${req.query.title}`;
+  }
   try {
     const comics = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}${filters}`
     );
 
     // console.log(comics.data);
