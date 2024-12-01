9b8d5cc98043e0e343b9d85ff4af1ab1f777c79e
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-12-01 21:47:00 +0100

    last commit with query of comics route and connecting database and added userRoute

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User");
+const User = require("../model/User");
 
 // packages pour gestion de mdp
 
